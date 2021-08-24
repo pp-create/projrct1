5c996915b7c3db663a588700c69796ee468a5c39
pp-create <73422779+pp-create@users.noreply.github.com>
2021-08-24 22:46:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class ArrayTest
+    {
+        static void Main()
+        {
+            //    {
+            //        // Declare the array of two elements.
+            //        int[][] arr = new int[2][];
+
+            //        // Initialize the elements.
+            //        arr[0] = new int[5] { 1, 3, 5, 7, 9 };
+            //        arr[1] = new int[4] { 2, 4, 6, 8 };
+
+            //        // Display the array elements.
+            //        for (int i = 0; i < arr.Length; i++)
+            //        {
+            //            System.Console.Write("Element({0}): ", i);
+
+            //            for (int j = 0; j < arr[i].Length; j++)
+            //            {
+            //                System.Console.Write("{0}{1}", arr[i][j], j == (arr[i].Length - 1) ? "" : " ");
+            //            }
+            //            System.Console.WriteLine();
+            //        }
+            //        // Keep the console window open in debug mode.
+            //        System.Console.WriteLine("Press any key to exit.");
+
+
+            //}
+
+
+
+            //{
+            //    //2
+            //    int n;
+            //    Console.WriteLine("please enter size array");
+            //    int.TryParse(Console.ReadLine(), out n);
+            //    int[] arr = new int[n];
+            //    for (int i = 0; i < arr.Length; i++)
+            //    {
+
+            //        int.TryParse(Console.ReadLine(), out arr[i]);
+
+            //    }
+            //    for (int i = 0; i < arr.Length; i++)
+            //    {
+
+
+            //        for (int j = n - 1; j > 0; j--)
+            //        {
+
+
+            //            if (arr[i] == arr[j])
+            //            {
+            //                int dis = (j - i) - 1;
+            //                Console.WriteLine("dis   " + dis);
+            //                return;
+            //            }
+
+
+            //        }
+
+            //    }
+            //}
+
+            //3----------------
+            //{
+            //    int n;
+            //    Console.WriteLine("please enter size array ?");
+            //    int.TryParse(Console.ReadLine(), out n);
+            //    string[] arr = new string[n];
+            //    for (int i = 0; i < arr.Length; i++)
+            //    {
+
+
+            //        arr[i] = Console.ReadLine();
+
+            //    }
+            //    for (int i = n - 1; i >= 0; i--)
+            //    {
+
+            //        Console.Write("" + arr[i] + "");
+
+            //        Console.WriteLine();
+            //    }
+            //}
+            //--------------
+            //{
+            //    string y = "";
+            //    int count = 0;
+            //    string x = "";
+            //    string[] arr = new string[1000000];
+
+            //    for (int i = 0; i < 1000000; i++)
+            //    {
+            //        x = i.ToString() + x;
+
+            //    }
+
+            //    //////Console.WriteLine(x);
+            //    for (int j = 0; j < x.Length; j++)
+            //    {
+            //        y = x.Substring(j, 1);
+
+            //        switch (y)
+            //        {
+            //            case "1":
+            //                count++;
+            //                break;
+
+            //            default:
+            //                break;
+            //        }
+            //    }
+
+            //    Console.WriteLine(count);
+
+
+            //}
+            //Console.WriteLine("plese enter the number of line");
+            //int line = int.Parse(Console.ReadLine());
+            //Console.WriteLine("plese enter the print");
+            //string prita = Console.ReadLine();
+            //Console.WriteLine();
+            //print(line, prita);
+        }
+
+
+        //public static void print(int line, string prita)
+        //{
+        //    for (int i = 1; i <= line; i++)
+        //    {
+        //        for (int J = 1; J <= i; J++)
+        //        {
+        //            Console.Write(prita);
+        //        }
+        //        Console.WriteLine();
+        //    }
+
+        //}
+    }
+    }
+
+            
+        
+
+
+
+
+
+
+
+
